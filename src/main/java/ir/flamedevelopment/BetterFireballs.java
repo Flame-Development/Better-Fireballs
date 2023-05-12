4d9afe72067c40f5c98e1deca0b183cd5f6fdce3
Mehran1022 <102294006+Mehran1022mm@users.noreply.github.com>
2023-05-12 19:14:59 +0330

    Add files via upload

---
+++ src/main/java/ir/flamedevelopment/BetterFireballs.java
@@ -0,0 +1,34 @@
+package ir.flamedevelopment;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterFireballs extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    @EventHandler
+    public void onPlayerInteract (PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = event.getItem();
+
+        if (event.getAction().toString().contains("RIGHT_CLICK") && item != null & item.getType() == Material.FIRE_CHARGE) {
+            event.setCancelled(true);
+            if (item.getAmount() > 1) {
+                item.setAmount(item.getAmount() - 1);
+            } else {
+                player.getInventory().removeItem(item);
+            }
+            Fireball fireball = player.launchProjectile(Fireball.class);
+            fireball.setYield(2);
+        }
+    }
+}
